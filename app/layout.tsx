aad549c5c8ba34a64beb479a81b05c74d463c8f0
capJavert <ante@kickass.website>
2022-11-28 12:57:12 +0100

    feat: add app homepage

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <html>
+            <head />
+            <body>{children}</body>
+        </html>
+    )
+}
+
+export default RootLayout
